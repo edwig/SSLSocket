2d27205e442f84eb7b73e2f51a9396bd55eb4847
Edwig Huisman <edwig.huisman@hetnet.nl>
2018-12-08 19:46:12 +0100

    Code & Comment cleanup

--- SSLSocket/Listener.cpp
+++ SSLSocket/Listener.cpp
@@ -68,9 +68,14 @@ UINT __cdecl Listener::Worker(void* p_argument)
   socket->m_clientCertAcceptable = listener->m_clientCertAcceptable;
 
 	SetThreadName("Connection Worker");
-	(listener->m_actualwork)((SocketStream*)transport->m_serverSocket);
-	delete transport;
-	return 0;
+
+  // Doing our work
+  listener->m_workerThreadCount++;
+ (listener->m_actualwork)((SocketStream*)transport->m_serverSocket);
+  listener->m_workerThreadCount--;
+
+  delete transport;
+  return 0;
 }
 
 // Worker process for connection listening
@@ -120,31 +125,27 @@ Listener::Initialize(int p_tcpListenPort)
 		// Only support PF_INET and PF_INET6.  If something else, skip to next address.
 		if ((AI->ai_family != AF_INET) && (AI->ai_family != AF_INET6)) continue;
 
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("::OnInit i = %d, ai_family = %d"), i, AI->ai_family);
-		// LogWarning(MsgText);
-
 		m_hSocketEvents[i] = CreateEvent(nullptr,   // no security attributes
 			                               true,		  // manual reset event
 			                               false,		  // not signaled
 			                               nullptr);	// no name
 
+    // Check that we got the event
     if(!(m_hSocketEvents[i]))
     {
       return UnknownError;
     }
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("::OnInit Created m_hSocketEvents[%d], handle=%d"), i, m_hSocketEvents[i]);
-		// LogWarning(MsgText);
 
+    // Create listen socket
 		m_listenSockets[i] = WSASocket(AI->ai_family, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
     if(m_listenSockets[i] == INVALID_SOCKET)
     {
       return SocketUnusable;
     }
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("::OnInit binding m_iListenSockets[%d] to sa_family=%u sa_data=%s len=%d"), i, AI->ai_addr->sa_family, AI->ai_addr->sa_data, AI->ai_addrlen);
-		// LogWarning(MsgText);
 
+    // Bind listen socket to this address
 		int rc = bind(m_listenSockets[i], AI->ai_addr, (int)AI->ai_addrlen);
-		if (rc)
+		if(rc)
 		{
       if(WSAGetLastError() == WSAEADDRINUSE)
       {
@@ -156,20 +157,20 @@ Listener::Initialize(int p_tcpListenPort)
       }
 		}
 
+    // Listen async on this socket
     if(listen(m_listenSockets[i],10))
     {
       return SocketUnusable;
     }
+    // Accept the FD_ACCEPT-ing event of the socket
     if(WSAEventSelect(m_listenSockets[i],m_hSocketEvents[i],FD_ACCEPT))
     {
       return SocketUnusable;
     }
 		i++;
 	}
+  // Register number of simultaneous sockets.
 	m_numListenSockets = i;
-
-	// StringCchPrintf(MsgText, _countof(MsgText), _T("::OnInit no errors, m_iNumListenSockets = %d"), m_iNumListenSockets);
-	// LogWarning(MsgText);
 
 	return NoError;
 }
@@ -199,33 +200,22 @@ void Listener::Listen(void)
 	HANDLE events[FD_SETSIZE+1];
 	SOCKET readSocket = NULL;
 	DWORD  wait       = 0;
-	//TCHAR MsgText[100];
 
-	m_workerThreadCount = 0;
+  m_workerThreadCount = 0;
 
 	DebugMsg("Start Listener::Listen method");
 
-	// StringCchPrintf(MsgText, _countof(MsgText), _T("Listener::Listen m_iNumListenSockets= %d"), m_iNumListenSockets);
-	// LogWarning(MsgText);
-
 	events[0] = m_stopEvent;
-	// StringCchPrintf(MsgText, _countof(MsgText), _T("Listener::Listen hEvents[0] = m_StopEvent = %d"), m_StopEvent);
-	// LogWarning(MsgText);
 
  	// Add the events for each socket type (two at most, one for IPv4, one for IPv6)
 	for (int i=0; i<m_numListenSockets; i++)
 	{
 		events[i+1] = m_hSocketEvents[i];
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("Listener::Listen hEvents[%d] = m_hSocketEvents[%d] = %d"), i+1, i, m_hSocketEvents[i]);
-		// LogWarning(MsgText);
 	}
 
 	// Loop until there is a problem or the shutdown event is caused
 	while (true)
 	{
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("Listener::Listen entering WaitForMultipleObjects for %d objects"), m_iNumListenSockets+1);
-		// LogWarning(MsgText);
-
 		wait = WaitForMultipleObjects(m_numListenSockets+1, events, false, INFINITE);
 
 		if (wait == WAIT_OBJECT_0)
@@ -234,8 +224,6 @@ void Listener::Listen(void)
 			break;
 		}
 		int iMyIndex = wait-1;
-		// StringCchPrintf(MsgText, _countof(MsgText), _T("Listener::Listen event %d triggered, iMyIndex = %d"), dwWait, iMyIndex);
-		// LogWarning(MsgText);
 
 		WSAResetEvent(m_hSocketEvents[iMyIndex]);
 		readSocket = accept(m_listenSockets[iMyIndex], 0, 0);
